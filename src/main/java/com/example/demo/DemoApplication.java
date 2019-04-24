e69c30d1262ae9a1964527f5e83ff468c1b925a4
Sanses <sooabia@gmail.com>
2019-04-24 16:41:00 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String hello(){
-        return "Hello Spring reactor docker!";
+        return "Hello Spring reactor docker! v2";
     }
     
 	public static void main(String[] args) {
